05c121c8cfca70038d5edb21c15098c43f602491
Hyeri Son <113450487+Hyeri1229@users.noreply.github.com>
2022-10-27 16:09:29 +0900

    Update 최댓값 만들기 （1）.java

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,3 +1,4 @@
+// 1번 풀이
 class Solution {
     public int solution(int[] numbers) {
         int max1 = 0;
@@ -11,6 +12,32 @@ class Solution {
 		}
 		
         int answer = max1 * max2;
+        return answer;
+    }
+}
+
+// 2번 풀이
+class Solution {
+    public int solution(int[] numbers) {
+	int max1 = 0;
+	int max2 = 0;
+	int index = 0;
+        int answer = 0;
+        
+	for (int i = 0; i < numbers.length; i++) {
+		if (max1 < numbers[i]) {
+			max1 = numbers[i];
+			index = i;
+		}
+	}
+
+	for (int i = 0; i < numbers.length; i++) {
+		if (i == index) continue;
+		if (max2 < numbers[i]) {
+			max2 = numbers[i];
+		}
+	}
+	answer = max1*max2;
         return answer;
     }
 }
