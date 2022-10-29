774ff152293d28b0895afe2a3fdd39a3b13c6483
Hyeri Son <113450487+Hyeri1229@users.noreply.github.com>
2022-10-30 07:09:45 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int pizza = 0;
+		
+		if (n % slice != 0) pizza =  n / slice + 1;
+		else pizza =  n / slice;
+        
+        return pizza;
+    }
+}
