81aff0e9b74e94a7fd9163167da0c9e28243ae62
Hyeri Son <113450487+Hyeri1229@users.noreply.github.com>
2022-11-07 12:20:01 +0900

    Update 문자 반복 출력하기.java

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -3,12 +3,12 @@ class Solution {
         String answer = "";
         char[] ch = new char[my_string.length()];
         
-        // str 배열에 String 한글자씩 넣기
+        // ch 배열에 String 한글자씩 넣기
         for (int i = 0; i < my_string.length(); i++) {
             ch[i] = my_string.charAt(i);
         }
         
-        // str 배열의 인덱스를 n번만큼 출력
+        // ch 배열의 인덱스를 n번만큼 출력
         for (int i = 0; i < ch.length; i++) {
             for (int j = 0; j < n; j++) {
                 answer += ch[i];
