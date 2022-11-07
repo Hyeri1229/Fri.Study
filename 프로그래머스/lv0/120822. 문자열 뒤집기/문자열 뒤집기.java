743c9008875d344ff5ae30183dc5585589d48ce2
Hyeri Son <113450487+Hyeri1229@users.noreply.github.com>
2022-11-07 12:27:36 +0900

    [level 0] Title: 문자열 뒤집기, Time: 18.38 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,8 +1,12 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
+        // index 변수를 String의 마지막 값으로 초기화
         int index = my_string.length()-1;
 		
+        // String 변수.charAt(index) -> String 변수의 index 값에 맞는 문자를 리턴해줌!
+        // index는 0부터 시작!
+        // answer에 String 문자열의 마지막 값부터 누적해서 입력 
 		for (int i = 0; i < my_string.length(); i++) {
 			answer += my_string.charAt(index);
 			index--;
