45bc8bf9f7fea06fd7376a9caa5c4aad65f1a10e
Hyeri Son <113450487+Hyeri1229@users.noreply.github.com>
2022-11-07 12:30:13 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -2,6 +2,9 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[2];
         
+        // num_list의 원소를 짝수인지 판별해서
+        // 짝수면 answer의 0번 인덱스에 카운트
+        // 홀수면 answer의 1번 인덱스에 카운트
         for (int i = 0; i < num_list.length; i++) {
             if (num_list[i] % 2 == 0) answer[0]++;
             else answer[1]++;
