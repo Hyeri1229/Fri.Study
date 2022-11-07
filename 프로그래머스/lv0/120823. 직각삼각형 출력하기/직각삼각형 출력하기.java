df9c86770c53ebd08addcf05daf9d39264889e51
Hyeri Son <113450487+Hyeri1229@users.noreply.github.com>
2022-11-07 12:29:07 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 129.03 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
@@ -6,6 +6,9 @@ public class Solution {
         int n = sc.nextInt();
         int star = 1;
         
+        // 정수 n이 입력되면 n번째 줄만큼 반복
+        // star 변수를 선언해서 1로 초기화
+        // 한 줄씩 증가할때마다 그 줄의 수만큼 별 출력
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < star; j++) {
                 System.out.print("*");                
