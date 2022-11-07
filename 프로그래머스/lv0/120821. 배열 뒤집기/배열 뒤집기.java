f73c83b69ac4403e41800097a5a593e28b27ff35
Hyeri Son <113450487+Hyeri1229@users.noreply.github.com>
2022-11-07 12:25:12 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,8 +1,11 @@
 class Solution {
     public int[] solution(int[] num_list) {
+        // 주어진 num_list와 같은 길이의 배열 선언
         int[] answer = new int[num_list.length];
+        // index 변수를 배열 제일 끝 값의 인덱스로 초기화
         int index = num_list.length-1;
         
+        // answer 배열을 num_list 배열의 끝값부터 넣기
         for (int i = 0; i < num_list.length; i++) {
             answer[i] = num_list[index];
             index--;
