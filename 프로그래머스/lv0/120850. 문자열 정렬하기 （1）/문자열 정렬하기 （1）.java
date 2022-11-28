71550b7fc08e8a6e58d3e9198896ff1e94c17425
Hyeri Son <113450487+Hyeri1229@users.noreply.github.com>
2022-11-28 14:06:52 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.26 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String my_string) {
+    public ArrayList solution(String my_string) {
         ArrayList<Integer> arr = new ArrayList<Integer>();
 		
         // my_string 길이만큼 for문 반복
@@ -12,16 +12,8 @@ class Solution {
 			if (temp >= '0' && temp <= '9') arr.add(Character.getNumericValue((char)temp));
 		}
 		
-        // arr에 담긴 값의 수만큼 길이 지정해서 int 배열 초기화
-		int[] result = new int[arr.size()];
-		
-        // int 배열에 arr 값 저장 (Arrays.sort 하기위해서!)
-		for (int i = 0; i < arr.size(); i++) {
-			result[i] = arr.get(i);
-		}
-		
-		Arrays.sort(result);
+        Collections.sort(arr);
         
-        return result;
+        return arr;
     }
 }
